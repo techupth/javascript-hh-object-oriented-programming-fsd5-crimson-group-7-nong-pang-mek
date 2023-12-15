1bb60746fa71b461f108b2b855a8acd95c25703d
nongparanai <promlerd.p@gmail.com>
2023-12-15 16:38:54 +0700

    fix(exercise): fix wrong commit

--- ex-2.js
+++ ex-2.js
@@ -1,39 +1,5 @@
-class Notification {
-  constructor(notificationId, createdTime, content, receiver) {
-    this.notificationId = notificationId;
-    this.createdTime = createdTime;
-    this.content = content;
-    this.receiver = receiver;
-  }
-  send() {
-    console.log(`Notification has been sent to ${this.receiver}`);
-  }
-}
+class Notification { }
 
-class EmailNotification extends Notification {
-  constructor(notificationId, createdTime, content, receiver) {
-    super(notificationId, createdTime, content, receiver);
-  }
-}
+class EmailNotification { }
 
-class SMSNotification extends Notification {
-  constructor(notificationId, createdTime, content, receiver) {
-    super(notificationId, createdTime, content, receiver);
-  }
-}
-
-const paranaiEmail = new EmailNotification(
-  "001",
-  new Date(),
-  "This is email notification message",
-  "papapaparrrranaiiii@gmail.com"
-);
-const paranaiSMS = new SMSNotification(
-  "001",
-  new Date(),
-  "This is SMS notification message",
-  "0987654321"
-);
-
-paranaiEmail.send();
-paranaiSMS.send();
+class SMSNotification { }
