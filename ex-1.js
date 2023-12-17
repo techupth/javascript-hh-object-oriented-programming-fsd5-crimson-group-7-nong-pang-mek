640a458c69b3f69cbc55ef7e99e85e4cb4a24d09
pang <pangton28.p@gmail.com>
2023-12-17 15:57:07 +0700

    feat(exercise): add completed oop

--- ex-1.js
+++ ex-1.js
@@ -1,3 +1,29 @@
-class EmailNotification { }
+class EmailNotification {
+constructor(notificationId, createdTime, content, receiver){
+    this.notificationId = notificationId;
+      this.createdTime = createdTime;
+      this.content = content;
+      this.receiver = receiver;
+}
+send() {
+console.log(`Notification has been sent to ${this.receiver}`);
+}  
+ }
 
-class SMSNotification { }
+class SMSNotification {
+    constructor(notificationId, createdTime, content, phoneNumber){
+        this.notificationId = notificationId;
+          this.createdTime = createdTime;
+          this.content = content;
+          this.phoneNumber = phoneNumber;
+ }
+ send() {
+console.log(`Notification has been sent to ${this.phoneNumber}`);
+}  
+}
+
+let email = new EmailNotification (1, new Date(), 'Your email has notifications.', 'pangton@gmail.com');
+let phoneNumber = new SMSNotification (1, new Date(), 'Your SMS has notifications.', '0912345678');
+
+email.send()
+phoneNumber.send()
--- ex-2.js
+++ ex-2.js
@@ -1,5 +1,21 @@
-class Notification { }
+class Notification {
+  constructor(notificationId, createdTime, content, receiver) {
+    this.notificationId = notificationId;
+    this.createdTime = createdTime;
+    this.content = content;
+    this.receiver = receiver;
+  }
+  send() {
+    console.log(`Notification has been sent to ${this.receiver}`);
+  }
+}
 
-class EmailNotification { }
+class EmailNotification extends Notification {}
 
-class SMSNotification { }
+class SMSNotification extends Notification {}
+
+let email = new EmailNotification (1, new Date(), 'Your email has notifications.', 'pangton@gmail.com');
+let phoneNumber = new SMSNotification (1, new Date(), 'Your SMS has notifications.', '0912345678');
+
+email.send()
+phoneNumber.send()
